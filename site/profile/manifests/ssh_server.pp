6e1f05cbf3e4aef4979e2e6a7eb67cc972ec2a49
MrDabangg <37298052+MrDabangg@users.noreply.github.com>
2018-12-25 11:52:57 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD1m6UhGnS0qvl9TduNDdH3mbcTYKSf1bWotQDwUAHYk2neTN0+zXRLLysikQF0rN1oD5sNMwglEDcmzaJTtD/Gp/lSG3e6eJ3Se2GNB+YXXohCKvZfii2s1J0UsjXaw1cxjXGD0CL//n72/JFqDTkEeRkh3p+6jMLux8PMqm7ky9LfqNan69FpLm4j65HufaBeCzafv6m2nFk+rVTezlaJf6aJbL4tO1EtPw1qJn80OT7PuVb1Hi5wnHzUoBy8ehbekNObjl+4LMOXqKWx9/maTns78QGGnEQs4V03I5+3Rdv4BvxWDv/eRxkpQVgBgwFitbktOQRbN2AwyLx1W++X',
   }  
 }
